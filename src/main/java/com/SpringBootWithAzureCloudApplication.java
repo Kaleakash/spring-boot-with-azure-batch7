3452bbaebef0c8d794a6d263e11ec442ca464579
akash <akash300383@gmail.com>
2022-06-29 18:27:09 +0530

    Spring boot project

---
+++ src/main/java/com/SpringBootWithAzureCloudApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithAzureCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithAzureCloudApplication.class, args);
+	}
+
+}
